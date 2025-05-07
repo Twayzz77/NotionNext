67307c7c6a8539e8dc403055e6dee9ff35c54413
Twayzz77 <110672121+Twayzz77@users.noreply.github.com>
2025-05-07 18:18:52 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,4 @@
-const BLOG = require('./blog.config')
-
-/**
- * 通常没啥用，sitemap交给 /pages/sitemap.xml.js 动态生成
- */
 module.exports = {
-  siteUrl: BLOG.LINK,
-  changefreq: 'daily',
-  priority: 0.7,
+  siteUrl: 'https://twayzz.com',
   generateRobotsTxt: true,
-  sitemapSize: 7000
-  // ...other options
-  // https://github.com/iamvishnusankar/next-sitemap#configuration-options
 }
